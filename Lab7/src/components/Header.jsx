983aedd14f8ef5e390b19eb31fecfbbd0712482d
Sychov Artem <78472265+jazzykman@users.noreply.github.com>
2025-12-11 23:03:28 +0200

    Add Header component with personal information

---
+++ Lab7/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <h2>Сичов Артем Євгенович</h2>
+      <p>Дата народження: 01.02.2006. Місце народження: Одеса</p>
+    </header>
+  );
+}
+
+export default Header;
